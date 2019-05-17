5f87aa650bdb221b14bfb0bbb7c2c649c10e3363
richglezriv <richglezriv@hotmail.com>
2019-05-17 18:13:02 -0500

    changes to profile payment

--- src/app/profile/payment/app-profile-payment.component.ts
+++ src/app/profile/payment/app-profile-payment.component.ts
@@ -7,4 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppProfilePaymentComponent{
   
+  years: number[] = [];
+
+  ngOnInit(){
+    const today = new Date().getFullYear();
+    
+    for(let i = today; i >= today - 10; i--){
+      this.years.push(i);
+    }
+    
+  }
 }
